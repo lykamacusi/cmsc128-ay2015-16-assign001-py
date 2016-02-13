bf8de819b9d86009266b44f6a09f8dbd8989b70e
lykamacusi <dgmacusi@up.edu.ph>
2016-02-13 21:42:06 +0800

    Documentation added for printMenu() and required functions added

--- cmsc128-ay2015-16-assign001.py
+++ cmsc128-ay2015-16-assign001.py
@@ -1,7 +1,37 @@
+#Accepts a whole number from zero(0) to 1 million(1000000 - without commas) and prints the number in word form
+def	numToWords():
+	print("Number to words");
+
+
+#-------------------------------------------------------------------------------------------------------------
+#Accepts a number in word form (from zero to 1 million) and returns it in numerical form
+#Input must be in lowercase
+def wordsToNum():
+	print("Words to number");
+
+
+#-------------------------------------------------------------------------------------------------------------
+#Accepts two arguments: the first argument is the number in word form (from zero to 1 million)
+#and the second argument isany of the following: JPY, PHP, USD.
+#The function returns the number in words to its numerical form with a prefix of the currency
+def wordsToCurency():
+	print("Words to currency");
+
+
+#-------------------------------------------------------------------------------------------------------------
+#Accepts three arguments: the first is the number from zero to 1 miliion,
+#the second is the delimiter to be used (single character only)
+#and third, the number of jumps when the delimiter will appear (from right most going to left most digit)
+def numberDelimited():
+	print("Number delimited");
+
+
+#-------------------------------------------------------------------------------------------------------------
+#Function for printing the menu
 def printMenu():
-	choice = 5;
+	choice = 5;	#initialize choice to 5
 	
-	while(choice != 0):
+	while(choice != 0):	#while choice is not equal to 0
 		print("***********************************");
 		print("*                                 *");
 		print("*     [ 1 ] Number to words       *");
@@ -12,16 +42,16 @@ def printMenu():
 		print("*                                 *");
 		print("***********************************");
 	
-		choice = int(input("Enter choice: ")) #Asks what the user wants to do
+		choice = int(input("Enter choice: ")) #asks what the user wants to do
 
 		if choice == 1:
-			print("1");
+			numToWords();	#calls the number to words function
 		elif choice == 2:
-			print("2");
+			wordsToNum();	#calls the words to number function
 		elif choice == 3:
-			print("3");
+			wordsToCurreny();	#calls the words to currency function
 		elif choice == 4:
-			print("4");
+			numberDelimited();	#calls the number delimited function
 		elif choice == 0:
 			print("\nProgram Terminated...");
 		else:
